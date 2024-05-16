660286854042afeabf77525d5d7d760d2c978979
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-05-16 21:34:58 +0200

    Added a reusable UIButton

--- Reusable Components/ReusableButton.swift
+++ Reusable Components/ReusableButton.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class ReusableButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configureUI()
+    }
+  
+    private func configureUI() {
+        self.layer.cornerRadius = 10
+        self.titleLabel?.textColor = .white
+        self.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+
 
 }
